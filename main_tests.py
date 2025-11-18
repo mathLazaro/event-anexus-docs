6d83665f7227a72fb7cdecf79436ba8b4b1b757c
Tomás Lavez <tomlavez@hotmail.com>
2025-11-18 10:53:02 -0300

    feat: Add login tests

--- main_tests.py
+++ main_tests.py
@@ -1,27 +1,49 @@
 import time
 from playwright.sync_api import sync_playwright
-from tests.login_test import fail_login, cadastro_organizador, cadastro_participante, login_organizador, login_participante
+from tests.login_test import (
+    esqueci_senha,
+    fail_login,
+    cadastro_organizador,
+    cadastro_participante,
+    login_organizador,
+    login_participante,
+)
+
+# from tests.event_test import cadastro_evento
+
 
 def main():
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
-    
+
+    # Dados de teste
+
+    email_organizador = "email3@teste.com"
+    senha_organizador = "senhaDeTeste@123"
+
+    email_participante = "email@teste.com"
+    senha_participante = "senhaDeTeste@123"
+
+    # Testes
     fail_login(page)
 
-    cadastro_organizador(page)
+    cadastro_organizador(email_organizador, senha_organizador, page)
 
-    login_organizador(page)
+    # esqueci_senha(email_organizador, senha_organizador, page)
 
-    cadastro_participante(page)
+    login_organizador(email_organizador, senha_organizador, page)
 
-    login_participante(page)
+    # cadastro_evento(page)
+
+    cadastro_participante(email_participante, senha_participante, page)
+
+    login_participante(email_participante, senha_participante, page)
 
     browser.close()
 
-    
-
     return
+
 
 if __name__ == "__main__":
     main()
--- tests/login_test.py
+++ tests/login_test.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 
+
 def fail_login(page: Page):
     page.goto("http://localhost:4200/login")
 
@@ -7,55 +8,64 @@ def fail_login(page: Page):
 
     page.locator('input[placeholder="Digite sua senha"]').fill("teste@teste.com")
 
-    page.locator('xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button').click()
-    
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button"
+    ).click()
+
     page.wait_for_timeout(5000)  # pausa 3 segundos
 
     page.screenshot(path="tests/img/fail_login.png")
 
-def login_participante(page: Page):
+
+def login_participante(email: str, senha: str, page: Page):
     page.goto("http://localhost:4200/login")
 
-    page.locator('input[placeholder="seu@email.com"]').fill("email@teste.com")
+    page.locator('input[placeholder="seu@email.com"]').fill(email)
 
-    page.locator('input[placeholder="Digite sua senha"]').fill("senhaDeTeste@123")
+    page.locator('input[placeholder="Digite sua senha"]').fill(senha)
 
-    page.locator('xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button').click()
-    
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button"
+    ).click()
+
     page.wait_for_timeout(5000)  # pausa 3 segundos
 
     page.screenshot(path="tests/img/login_participante.png")
 
 
-def login_organizador(page: Page):
+def login_organizador(email: str, senha: str, page: Page):
     page.goto("http://localhost:4200/login")
 
-    page.locator('input[placeholder="seu@email.com"]').fill("email2@teste.com")
+    page.locator('input[placeholder="seu@email.com"]').fill(email)
 
-    page.locator('input[placeholder="Digite sua senha"]').fill("senhaDeTeste@123")
+    page.locator('input[placeholder="Digite sua senha"]').fill(senha)
 
-    page.locator('xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button').click()
-    
-    page.wait_for_timeout(5000)  # pausa 3 segundos
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[1]/button"
+    ).click()
+
+    page.wait_for_timeout(3000)  # pausa 3 segundos
 
     page.screenshot(path="tests/img/login_organizador.png")
 
-def cadastro_participante(page: Page):
+
+def cadastro_participante(email: str, senha: str, page: Page):
     # Página inicial
     page.goto("http://localhost:4200/login")
 
-    page.locator('xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[2]/button').click()
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[2]/button"
+    ).click()
 
     # Página cadastro
-    page.locator('input[name="name"]').fill('Nome de teste')
+    page.locator('input[name="name"]').fill("Nome de teste")
 
-    page.locator('app-cadastro input[name="email"]').fill('email@teste.com')
+    page.locator('app-cadastro input[name="email"]').fill(email)
 
-    page.locator('input[name="telephone_number"]').fill('12345678910')
+    page.locator('input[name="telephone_number"]').fill("12345678910")
 
-    page.locator('app-cadastro input[name="password"]').fill('senhaDeTeste@123')
-
-    page.locator('input[name="confirmPassword"]').fill('senhaDeTeste@123')
+    page.locator('app-cadastro input[name="password"]').fill(senha)
+    page.locator('input[name="confirmPassword"]').fill(senha)
 
     page.get_by_role("button", name="Cadastrar").click()
 
@@ -63,22 +73,24 @@ def cadastro_participante(page: Page):
 
     page.screenshot(path="tests/img/cadastro_participante.png")
 
-def cadastro_organizador(page: Page):
+
+def cadastro_organizador(email: str, senha: str, page: Page):
     # Página inicial
     page.goto("http://localhost:4200/login")
 
-    page.locator('xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[2]/button').click()
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[3]/app-button[2]/button"
+    ).click()
 
     # Página cadastro
-    page.locator('input[name="name"]').fill('Nome de teste 2')
+    page.locator('input[placeholder="Digite seu nome"]').fill("Nome de teste 2")
 
-    page.locator('app-cadastro input[name="email"]').fill('email2@teste.com')
+    page.locator('input[placeholder="seu@email.com"]').fill(email)
 
-    page.locator('input[name="telephone_number"]').fill('12345678910')
+    page.locator('input[placeholder="(00) 00000-0000"]').fill("12345678910")
 
-    page.locator('app-cadastro input[name="password"]').fill('senhaDeTeste@123')
-
-    page.locator('input[name="confirmPassword"]').fill('senhaDeTeste@123')
+    page.locator('input[placeholder="Mínimo 8 caracteres"]').fill(senha)
+    page.locator('input[placeholder="Digite a senha novamente"]').fill(senha)
 
     page.get_by_label("Organizador").check()
 
@@ -87,3 +99,15 @@ def cadastro_organizador(page: Page):
     page.wait_for_timeout(5000)  # pausa 3 segundos
 
     page.screenshot(path="tests/img/cadastro_organizador.png")
+
+
+def esqueci_senha(email: str, page: Page):
+    page.goto("http://localhost:4200/login")
+
+    page.locator(
+        "xpath=/html/body/app-root/div/app-auth-shell/main/app-login/section/div/div[2]/form/div[2]/a"
+    ).click()
+
+    page.locator('input[placeholder="seu@email.com"]').fill(email)
+
+    page.locator('button[type="submit"]').click()
